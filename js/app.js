b845ef18282e7c70b96370d340ec436f3dadcd29
Benny Ho <benny.ho2530@gmail.com>
2020-02-20 19:04:57 +0100

    added global variables

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// global variables
+
+const cartInfoBtn = document.querySelector('#cart-info');
+
+const itemCount = document.querySelector('#item-count');
+const itemTotal = document.querySelector('#item-total');
+
+
+const cartText = document.querySelector('#cart');
+const itemImg = document.querySelector('#item-img');
+const itemTitle = document.querySelector('#cart-item-title');
+const itemPrice = document.querySelector('#cart-item-price');
+const itemRemove = document.querySelector('#cart-item-remove');
+
+const cartTotal = document.querySelector('#cart-total');
+const cartClear = document.querySelector('#clear-cart');
+
+
+const storeImg = document.querySelector('.store-img');
+
+// cart info function
+
+// click icon and add function
