569a20bebde434f9bcf49e3434253db44b71d451
Benny Ho <benny.ho2530@gmail.com>
2020-02-21 15:22:51 +0100

    getting values of prices and img src

--- js/app.js
+++ js/app.js
@@ -1,3 +1,112 @@
+// show cart
+
+(function() {
+
+const cartInfo = document.querySelector('#cart-info');
+const cart = document.querySelector('#cart');
+
+cartInfo.addEventListener('click', function() {
+
+    cart.classList.toggle('show-cart');
+    console.log('hello');
+})
+})();
+
+// add items to the cart
+
+(function() {
+
+
+const cartBtn = document.querySelectorAll('.store-item-icon');
+
+
+cartBtn.forEach(btn => {
+    btn.addEventListener('click', function(event) {
+        
+      //  console.log(event.target) --> check what is clicked
+
+        
+       if(event.target.parentElement.classList.contains('store-item-icon')) {
+         
+        let fullPath = event.target.parentElement.previousElementSibling.src;
+        console.log('check fullPath: ' + fullPath);
+        let pos = fullPath.indexOf('img') + 3;
+        console.log('check pos: ' + pos);
+        let partPath = fullPath.slice(pos);
+        console.log('check partPath:' + partPath)
+
+        const item = {};
+        item.img = `img-cart${partPath}`;
+
+        let name = event.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
+        item.name = name;
+
+        let price = event.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
+        
+        let finalPrice = price.slice(1).trim();
+
+        item.price = finalPrice;
+        
+
+        // console.log(name)
+
+
+       //  console.log(item);
+       }
+       
+       
+        // alert('Item added the cart')
+        
+        // gather value of items and picture
+       
+        // addItem(btn)
+      
+        // console.log('check itemPrice: ' + itemPrice);
+
+        // add the values and pictures in cart
+    })
+})
+
+/*
+function addItem(button) {
+
+    const itemNames = document.querySelectorAll('#store-item-name');
+    const itemPrices = document.querySelectorAll('#store-item-price');
+    const itemImg = document.querySelector('.store-img');
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach(card => {
+
+        button.addEventListener('click', function() {
+
+            console.log(card.querySelector('#store-item-name').textContent)
+            console.log(card.querySelector('#store-item-price').textContent)
+        })
+
+        })
+       
+    /*
+    itemNames.forEach(item => {
+
+       console.log(item.textContent)
+    })
+
+    itemPrices.forEach(item => {
+        console.log(item.textContent)
+    })
+    */
+    /*
+    console.log(itemName.textContent)
+    console.log(itemPrice.textContent)
+    
+
+   
+}
+*/
+})();
+
+
+/*
 // global variables
 
  const cartInfoBtn = document.querySelector('#cart-info');
@@ -28,3 +137,5 @@ cartInfoBtn.addEventListener('click', function() {
 })
 
 // click icon and add function
+
+*/
