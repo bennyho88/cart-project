6d15e2b4e1504a89a3b23a047c32d5f3e6726f7d
Benny Ho <benny.ho2530@gmail.com>
2020-02-21 22:30:28 +0100

    modified functions

--- js/app.js
+++ js/app.js
@@ -17,6 +17,95 @@ cartInfo.addEventListener('click', function() {
 (function() {
 
 
+
+const cartBtn = document.querySelectorAll('.store-item-icon');
+
+cartBtn.forEach(btn => {
+    btn.addEventListener('click', function(event) {
+       
+        if (event.target.parentElement.classList.contains('store-item-icon')) {
+
+            let fullPath = event.target.parentElement.parentElement.firstElementChild.src;
+            let pos = fullPath.indexOf('img') + 3;
+            let defPath = fullPath.slice(pos);
+            
+
+            const item = {};
+
+            item.img = `img-cart${defPath}`;
+
+         let name = event.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
+
+         item.name = name;
+
+         let price = event.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
+
+        const finalPrice = price.slice(1).trim();
+
+        item.price = finalPrice
+     
+            console.log(item);
+
+            const cartItem = document.createElement('div');
+            cartItem.classList.add('cart-item', 'd-flex', 'justify-content-between', 'text-capitalize', 'my-3');
+            cartItem.innerHTML = `
+            <img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
+            <div class="cart-item-text">
+
+              <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
+              <span>$</span>
+              <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
+            </div>
+            <a href="#" id='cart-item-remove' class="cart-item-remove">
+              <i class="fas fa-trash"></i>
+            </a>
+          </div>
+            `;
+
+          const total = document.querySelector('.cart-total-container');
+          const cart = document.querySelector('#cart')
+
+          cart.insertBefore(cartItem, total);
+          alert('item added the cart')
+
+          showTotals();
+
+
+
+        }
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const cartBtn = document.querySelectorAll('.store-item-icon');
 
 
@@ -81,7 +170,7 @@ cartBtn.forEach(btn => {
        }
     })
 })
-
+*/
 // show totals
 
 function showTotals() {
@@ -98,7 +187,7 @@ function showTotals() {
    // console.log(total);
 
    const totalMoney = total.reduce(function(total, item) {
-      
+     
     total += item;
     
     return total;
@@ -117,76 +206,6 @@ function showTotals() {
    itemTotal.textContent = finalMoney;
    itemCount.textContent = total.length;
 }
-
-/*
-function addItem(button) {
-
-    const itemNames = document.querySelectorAll('#store-item-name');
-    const itemPrices = document.querySelectorAll('#store-item-price');
-    const itemImg = document.querySelector('.store-img');
-    const cards = document.querySelectorAll('.card');
-
-    cards.forEach(card => {
-
-        button.addEventListener('click', function() {
-
-            console.log(card.querySelector('#store-item-name').textContent)
-            console.log(card.querySelector('#store-item-price').textContent)
-        })
-
-        })
-       
-    /*
-    itemNames.forEach(item => {
-
-       console.log(item.textContent)
-    })
-
-    itemPrices.forEach(item => {
-        console.log(item.textContent)
-    })
-    */
-    /*
-    console.log(itemName.textContent)
-    console.log(itemPrice.textContent)
-    
-
-   
-}
-*/
 })();
 
 
-/*
-// global variables
-
- const cartInfoBtn = document.querySelector('#cart-info');
-// const cartInfoBtn = document.querySelector('.navbar-toggler');
-
-
-const itemCount = document.querySelector('#item-count');
-const itemTotal = document.querySelector('#item-total');
-
-
-const cartText = document.querySelector('#cart');
-const itemImg = document.querySelector('#item-img');
-const itemTitle = document.querySelector('#cart-item-title');
-const itemPrice = document.querySelector('#cart-item-price');
-const itemRemove = document.querySelector('#cart-item-remove');
-
-const cartTotal = document.querySelector('#cart-total');
-const cartClear = document.querySelector('#clear-cart');
-
-
-const storeImg = document.querySelector('.store-img');
-
-// cart info function
-
-cartInfoBtn.addEventListener('click', function() {
-    console.log('hello');
-    cartText.classList.toggle('show')
-})
-
-// click icon and add function
-
-*/
