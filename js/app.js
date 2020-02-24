75cf459e0dcdd3f9a4a8c2423014512ff61ae477
Benny Ho <benny.ho2530@gmail.com>
2020-02-24 13:32:27 +0100

    refactored code

--- js/app.js
+++ js/app.js
@@ -15,43 +15,48 @@ cartInfo.addEventListener('click', function() {
 // add items to the cart
 
 (function() {
+  const cartBtn = document.querySelectorAll('.store-item-icon');
 
 
-
-const cartBtn = document.querySelectorAll('.store-item-icon');
-
-cartBtn.forEach(btn => {
-    btn.addEventListener('click', function(event) {
-       
-        if (event.target.parentElement.classList.contains('store-item-icon')) {
-
-            let fullPath = event.target.parentElement.parentElement.firstElementChild.src;
-            let pos = fullPath.indexOf('img') + 3;
-            let defPath = fullPath.slice(pos);
-            
-
-            const item = {};
-
-            item.img = `img-cart${defPath}`;
-
-         let name = event.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
-
-         item.name = name;
-
-         let price = event.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
-
-        const finalPrice = price.slice(1).trim();
-
-        item.price = finalPrice
-     
-            console.log(item);
-
-            const cartItem = document.createElement('div');
-            cartItem.classList.add('cart-item', 'd-flex', 'justify-content-between', 'text-capitalize', 'my-3');
-            cartItem.innerHTML = `
-            <img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
-            <div class="cart-item-text">
-
+  cartBtn.forEach(btn => {
+      btn.addEventListener('click', function(event) {
+          
+        //  console.log(event.target) --> check what is clicked
+  
+          
+         if(event.target.parentElement.classList.contains('store-item-icon')) {
+           
+          let fullPath = event.target.parentElement.previousElementSibling.src;
+          console.log('check fullPath: ' + fullPath);
+          let pos = fullPath.indexOf('img') + 3;
+          console.log('check pos: ' + pos);
+          let partPath = fullPath.slice(pos);
+          console.log('check partPath:' + partPath)
+  
+          const item = {};
+          item.img = `img-cart${partPath}`;
+  
+          let name = event.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
+          item.name = name;
+  
+          let price = event.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
+          
+          let finalPrice = price.slice(1).trim();
+  
+          item.price = finalPrice;
+          
+  
+          // console.log(name)
+         //  console.log(item);
+  
+  
+          const cartItem = document.createElement('div');
+          cartItem.classList.add('cart-item', 'd-flex', 'justify-content-between', 'text-capitalize', 'my-3')
+  
+          cartItem.innerHTML = `
+          <img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
+            <div class="item-text">
+  
               <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
               <span>$</span>
               <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
@@ -59,153 +64,58 @@ cartBtn.forEach(btn => {
             <a href="#" id='cart-item-remove' class="cart-item-remove">
               <i class="fas fa-trash"></i>
             </a>
-          </div>
-            `;
-
-          const total = document.querySelector('.cart-total-container');
-          const cart = document.querySelector('#cart')
-
-          cart.insertBefore(cartItem, total);
-          alert('item added the cart')
-
-          showTotals();
-
-
-
-        }
-    })
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const cartBtn = document.querySelectorAll('.store-item-icon');
-
-
-cartBtn.forEach(btn => {
-    btn.addEventListener('click', function(event) {
-        
-      //  console.log(event.target) --> check what is clicked
-
-        
-       if(event.target.parentElement.classList.contains('store-item-icon')) {
-         
-        let fullPath = event.target.parentElement.previousElementSibling.src;
-        console.log('check fullPath: ' + fullPath);
-        let pos = fullPath.indexOf('img') + 3;
-        console.log('check pos: ' + pos);
-        let partPath = fullPath.slice(pos);
-        console.log('check partPath:' + partPath)
-
-        const item = {};
-        item.img = `img-cart${partPath}`;
-
-        let name = event.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
-        item.name = name;
-
-        let price = event.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
-        
-        let finalPrice = price.slice(1).trim();
-
-        item.price = finalPrice;
-        
-
-        // console.log(name)
-       //  console.log(item);
-
-
-        const cartItem = document.createElement('div');
-        cartItem.classList.add('cart-item', 'd-flex', 'justify-content-between', 'text-capitalize', 'my-3')
-
-        cartItem.innerHTML = `
-        <img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
-          <div class="item-text">
-
-            <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
-            <span>$</span>
-            <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
-          </div>
-          <a href="#" id='cart-item-remove' class="cart-item-remove">
-            <i class="fas fa-trash"></i>
-          </a>
-          </div>`;
-        
-        // select card
-
-        const card = document.querySelector('#cart');
-        const total = document.querySelector('.cart-total-container');
-
-        card.insertBefore(cartItem, total);
-        alert('item added to the cart');
-
-        showTotals();
-
-       }
-    })
-})
-*/
-// show totals
-
-function showTotals() {
-    
-    const total = [];
-    const items = document.querySelectorAll('.cart-item-price');
-
+            </div>`;
+          
+          // select card
   
-    items.forEach(item => {
-        // parsefloat method for converting string to number
-        total.push(parseFloat(item.textContent));
-    })
-
-   // console.log(total);
-
-   const totalMoney = total.reduce(function(total, item) {
-     
-    total += item;
+          const card = document.querySelector('#cart');
+          const total = document.querySelector('.cart-total-container');
+  
+          card.insertBefore(cartItem, total);
+          alert('item added to the cart');
+  
+          showTotals();
+  
+         }
+      })
+  })
+  
+  
+  // show totals
+  
+  function showTotals() {
+      
+      const total = [];
+      const items = document.querySelectorAll('.cart-item-price');
+  
     
-    return total;
-   }, 0);
-
-   const finalMoney = totalMoney.toFixed(2);
-
-   console.log(finalMoney);
-
-   let cartTotal = document.querySelector('#cart-total');
-   cartTotal.textContent = finalMoney;
-
-   let itemCount = document.querySelector('#item-count');
-   let itemTotal = document.querySelector('.item-total');
-
-   itemTotal.textContent = finalMoney;
-   itemCount.textContent = total.length;
-}
+      items.forEach(item => {
+          // parsefloat method for converting string to number
+          total.push(parseFloat(item.textContent));
+      })
+  
+     // console.log(total);
+  
+     const totalMoney = total.reduce(function(total, item) {
+       
+      total += item;
+      
+      return total;
+     }, 0);
+  
+     const finalMoney = totalMoney.toFixed(2);
+  
+     console.log(finalMoney);
+  
+     let cartTotal = document.querySelector('#cart-total');
+     cartTotal.textContent = finalMoney;
+  
+     let itemCount = document.querySelector('#item-count');
+     let itemTotal = document.querySelector('.item-total');
+  
+     itemTotal.textContent = finalMoney;
+     itemCount.textContent = total.length;
+  }
 })();
 
 
